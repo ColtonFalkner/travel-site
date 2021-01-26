4df05e9d2e4d2b657881c2392e2131784f33b4ed
Colton <coltonfalkner@gmail.com>
2021-01-26 12:47:18 -0500

    new module creatd and index updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
+
 const postCSSPlugins = [
   require('postcss-import'),
   require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
+  require('postcss-hexrgba'),
   require('autoprefixer'),
 ]
+
 module.exports = {
   entry: './app/assets/scripts/App.js',
   output: {
@@ -13,13 +16,13 @@ module.exports = {
     path: path.resolve(__dirname, 'app'),
   },
   devServer: {
+    before: function (app, server) {
+      server._watch('./app/**/*.html')
+    },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
     port: 3000,
     host: '0.0.0.0',
-    before: function (app, server) {
-      server._watch('./app/**/*.html')
-    },
   },
   mode: 'development',
   module: {
@@ -29,10 +32,7 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader?url=false',
-          {
-            loader: 'postcss-loader',
-            options: { postcssOptions: { plugins: postCSSPlugins } },
-          },
+          { loader: 'postcss-loader', options: { plugins: postCSSPlugins } },
         ],
       },
     ],
