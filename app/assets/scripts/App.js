ebcfe7a2ac6a73a91660cf96bcb37f0bf12856fa
Colton <coltonfalkner@gmail.com>
2021-01-28 16:12:43 -0500

    modularity has been introduced

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,21 @@
 import '../styles/styles.css'
+import Person from './modules/Person.js'
 
 if (module.hot) {
   module.hot.accept()
 }
+
+////////////////////////
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + ' now owes 0 in taxes')
+  }
+}
+
+var John = new Person('John Doe', 'blue')
+John.greet()
+
+let Jane = new Adult('Jane Smith', 'green')
+Jane.greet()
+Jane.payTaxes()
