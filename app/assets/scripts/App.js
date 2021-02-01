6ca7b40414c7697774ec0e8d43a0b72da18bcacc
Colton <coltonfalkner@gmail.com>
2021-02-01 10:53:17 -0500

    Javascripting has begun.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,8 @@
 import '../styles/styles.css'
-import Person from './modules/Person.js'
+import MobileMenu from './modules/MobileMenu'
+
+let mobileMenu = new MobileMenu()
 
 if (module.hot) {
   module.hot.accept()
 }
-
-////////////////////////
-
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + ' now owes 0 in taxes')
-  }
-}
-
-var John = new Person('John Doe', 'blue')
-John.greet()
-
-let Jane = new Adult('Jane Smith', 'green')
-Jane.greet()
-Jane.payTaxes()
