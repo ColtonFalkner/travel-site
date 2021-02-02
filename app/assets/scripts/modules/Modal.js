5f0a43266b47551352199cef7a3151e0b8b64b8c
Colton <coltonfalkner@gmail.com>
2021-02-02 17:03:13 -0500

    bout that lazy loading

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,23 +3,18 @@ class Modal {
     this.injectHTML()
     this.modal = document.querySelector('.modal')
     this.closeIcon = document.querySelector('.modal__close')
-    this.openModalButtons = document.querySelectorAll('.open-modal')
+
     this.events()
   }
 
   events() {
-    //listen for open click
-    this.openModalButtons.forEach((el) =>
-      el.addEventListener('click', (e) => this.openTheModal(e))
-    )
     //listen for closing click
     this.closeIcon.addEventListener('click', () => this.closeTheModal())
     //pushes any key
     document.addEventListener('keyup', (e) => this.keyPressHandler(e))
   }
 
-  openTheModal(e) {
-    e.preventDefault()
+  openTheModal() {
     this.modal.classList.add('modal--is-visible')
   }
 
