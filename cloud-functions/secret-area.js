c9b6110e280aa8876ed76693e5186bbff45a290d
Colton <coltonfalkner@gmail.com>
2021-02-04 16:28:47 -0500

    hopefully fixed json parse error

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,15 +1,13 @@
-const { parse } = require('postcss')
-
 exports.handler = function (event, context, callback) {
   const secretContent = `
-        <h3> Welcome to the Secret Area </h3>
-        <p>Here we can tell you that the sky is <strong>blue</strong> and you aren't holding the bag on GME</p>
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
 
   let body
 
   if (event.body) {
-    body = JSON.parse('event.body')
+    body = JSON.parse(event.body)
   } else {
     body = {}
   }
